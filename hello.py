2309cefedb9caffdd193d6df3dc056d355103973
Glimmer <hakaishiny@gmail.com>
2026-01-29 20:38:15 +0900

    挨拶処理の関数名を変更。また2倍する関数を追加。

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
-def greet(name):
-    return f"Hello, {name}! This is a branch version."
+def greet_function(name):
+    return f"Hello, {name}! This is a branch version 3."
+
+
+def calculator(number):
+    return number*2
 
 
 if __name__ == "__main__":
