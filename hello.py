023f09e47b9724d03d8d6c3bf5a4123ca7916ec4
Glimmer <hakaishiny@gmail.com>
2026-01-29 18:24:56 +0900

    ブランチで挨拶文を変更

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
-# hello.py
 def greet(name):
-    return f"Hello, {name}! Welcome to GitHub!"
+    return f"Hello, {name}! This is a branch version."
+
 
 if __name__ == "__main__":
     print(greet("GitHub"))
